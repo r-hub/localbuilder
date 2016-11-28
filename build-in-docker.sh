7d3e44888ce192f0c497f21eaca8a4723009e1a2
Gábor Csárdi <csardi.gabor@gmail.com>
2016-11-28 17:16:36 +0000

    Some progress messages

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -4,28 +4,37 @@ set -e
 
 package=$1
 
+echo "Container running"
+
 export PATH=$(ls /opt/R-* -d)/bin:$PATH
 echo "options(repos = c(CRAN = \"https://cran.r-hub.io/\"))" >> ~/.Rprofile
 
 # Download source package and extract it
+echo "Downloading source package"
 pkgfile=$(Rscript -e 'p <- download.packages("'$package'", "."); cat(p[,2])')
 tar xzf $pkgfile
 
 # Install the sysreqs package
+echo "Installing sysreqs package"
 Rscript -e 'source("https://install-github.me/r-hub/sysreqs")'
 
 # Get system requirements
+echo "Querying system requirements"
 (
     cd $package
     sysreqs=$(Rscript -e "cat(sysreqs::sysreq_commands(\"DESCRIPTION\", soft = FALSE))")
     if [[ ! -z "$sysreqs" ]]; then
 	echo "$sysreqs" > sysreqs.sh
 	chmod +x sysreqs.sh
+	echo "Installing system requirements"
 	./sysreqs.sh
+    else
+	echo "No system requirements are needed"
     fi
 )
 
 # Install package and create a binary from it
+echo "Installing package (and building binary)"
 R CMD INSTALL --build $package
 
 # Put down the filename in a file
--- make-binary-package.sh
+++ make-binary-package.sh
@@ -10,6 +10,7 @@ package=$2
 if [[ -z "$image" || -z "$package" ]]; then usage; exit 1; fi
 
 dir=$(pwd)
+echo "Starting container"
 cont=$(docker run -d -t -v "${dir}/build-in-docker.sh":/build-in-docker.sh \
 	      ${image} bash /build-in-docker.sh ${package})
 
@@ -19,11 +20,13 @@ docker attach $cont || true
 status=$(docker inspect $cont | grep ExitCode | cut -d: -f2 | \
 		sed 's/[^0-9]//g')
 
-if [[ "$status" == 0 ]]; then 
+if [[ "$status" == 0 ]]; then
+    echo "Copy binary package from container"
     docker cp "${cont}:output_file" .
     output=$(cat output_file)
     docker cp "${cont}:${output}" .
     exit 0
 else
+    echo "Build failed, no binary package was produced"
     exit 1
 fi
