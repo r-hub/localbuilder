75c6161b5a2a2f7a2348cae93e741b63d3c04b08
Gábor Csárdi <csardi.gabor@gmail.com>
2016-11-28 17:31:46 +0000

    Use container name instead of detach/attach
    We do not lose the output, and it is more reliable, anyway.

--- make-binary-package.sh
+++ make-binary-package.sh
@@ -9,22 +9,24 @@ package=$2
 
 if [[ -z "$image" || -z "$package" ]]; then usage; exit 1; fi
 
+contid=$(cat /dev/urandom | LC_CTYPE=C  tr -dc 'a-zA-Z0-9' |
+		fold -w 32 | head -n 1)
+
 dir=$(pwd)
 echo "Starting container"
-cont=$(docker run -d -t -v "${dir}/build-in-docker.sh":/build-in-docker.sh \
-	      ${image} bash /build-in-docker.sh ${package})
-
-docker attach $cont || true
+docker run -t -v "${dir}/build-in-docker.sh":/build-in-docker.sh \
+       --name $contid ${image} \
+       bash /build-in-docker.sh ${package}
 
 ## Get exit status
-status=$(docker inspect $cont | grep ExitCode | cut -d: -f2 | \
+status=$(docker inspect $contid | grep ExitCode | cut -d: -f2 | \
 		sed 's/[^0-9]//g')
 
 if [[ "$status" == 0 ]]; then
     echo "Copy binary package from container"
-    docker cp "${cont}:output_file" .
+    docker cp "${contid}:output_file" .
     output=$(cat output_file)
-    docker cp "${cont}:${output}" .
+    docker cp "${contid}:${output}" .
     exit 0
 else
     echo "Build failed, no binary package was produced"
