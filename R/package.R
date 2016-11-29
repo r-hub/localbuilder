f94b951dedd41f6b638219bdc4feb840f8d43f9c
Gábor Csárdi <csardi.gabor@gmail.com>
2016-11-29 17:48:40 +0000

    Fix up mason

--- R/package.R
+++ R/package.R
@@ -1,8 +1,8 @@
 
 #' Build R Binary Packages for Multiple Platforms
 #'
-#' Currently it supports Linux via Docker. It uses R-hub's Docker images to build binary R packages for
-  various platforms.
+#' Currently it supports Linux via Docker. It uses R-hub's Docker images
+#' to build binary R packages for various platforms.
 #'
 #' @docType package
 #' @name localbuilder
