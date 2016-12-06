151e24692eeef3a47319c915135c13e19fb39c0a
Gábor Csárdi <csardi.gabor@gmail.com>
2016-12-06 17:08:52 +0000

    Starting to write save, adjust messages

--- R/save.R
+++ R/save.R
@@ -1,4 +1,15 @@
 
-save_binary_to_repo <- function(path, container_id) {
-  
+save_binary_to_repo <- function(path, container_id, user) {
+
+  ## List files in the container, and the copy out what we need
+  new_id <- random_id()
+  files <- docker_run(
+    image_id,
+    name = new_id,
+    user = user,
+    workdir = paste0("/home/", user),
+    command = c("bash", "-l", "-c", "ls")
+  )$stdout
+
+  files
 }
